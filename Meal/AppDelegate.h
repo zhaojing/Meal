af165036e370e32236b608407061d85d05d12907
jingzhao <jingzhao@thoughtworks.com>
2016-08-04 16:28:16 +0800

    Initial Commit

---
+++ Meal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Meal
+//
+//  Created by JingZhao on 8/4/16.
+//  Copyright © 2016 JingZhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Meal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Meal
+//
+//  Created by JingZhao on 8/4/16.
+//  Copyright © 2016 JingZhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
