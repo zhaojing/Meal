bda9918e9593e2a00ecd53f233aecd73a4f5e4be
fanwise <fanwise.901@gmail.com>
2016-08-06 23:58:07 +0800

    add a motionBegan method, which can show random menu in log

--- Meal/Shake/ShakeViewController.m
+++ Meal/Shake/ShakeViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ShakeViewController.h"
+#import "MenuRequest.h"
+#import "Menu.h"
 
 @interface ShakeViewController ()
+
+@property (strong, nonatomic) MenuRequest *request;
 
 @end
 
@@ -16,22 +20,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - ShakeAction
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void) motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (motion == UIEventSubtypeMotionShake) {
+        self.request = [[MenuRequest alloc] init];
+        NSArray *allMenus = [self.request getAllMenus];
+        Menu *randomMenu = allMenus[arc4random() % [allMenus count]];
+        NSLog(@"menuId:%ld name:%@ price:%@ location:%@",
+              (long)randomMenu.menuId,
+              randomMenu.name,
+              randomMenu.price,
+              randomMenu.location);
+    }
 }
-*/
 
 @end
