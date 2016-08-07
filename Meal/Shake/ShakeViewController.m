949a58fe79ebe0259b31cded1f762901d14a82c0
fanwise <fanwise.901@gmail.com>
2016-08-07 14:16:03 +0800

    add shake in storyboard now can display shake result in screen

--- Meal/Shake/ShakeViewController.m
+++ Meal/Shake/ShakeViewController.m
@@ -15,35 +15,62 @@
 
 @property (strong, nonatomic) MenuRequest *request;
 
+@property (strong, nonatomic) IBOutlet UIImageView *resultImage;
+@property (strong, nonatomic) IBOutlet UILabel *resultName;
+@property (strong, nonatomic) IBOutlet UILabel *resultPrice;
+@property (strong, nonatomic) IBOutlet UILabel *resultLocation;
+@property ShakeStatus currentShakeStatus;
+
 @end
 
 @implementation ShakeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self cleanUIForShakeBegin];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
+- (void)cleanUIForShakeBegin {
+    self.currentShakeStatus = beginToShake;
+    self.resultImage.image = [UIImage imageNamed:@"dishes.png"];
+    [self.resultName setHidden:true];
+    [self.resultPrice setHidden:true];
+    [self.resultLocation setHidden:true];
+}
+
 #pragma mark - ShakeAction
 
-- (void) motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event {
-    if (motion == UIEventSubtypeMotionShake) {
-        self.request = [[MenuRequest alloc] init];
-        NSArray *allMenus = [self.request getAllMenus];
-        if([allMenus count] == 0) {
-            [SVProgressHUD showErrorWithStatus: @"还没有选项，请先添加一些"];
-        }else{
-            Menu *randomMenu = allMenus[arc4random() % [allMenus count]];
-            NSLog(@"menuId:%ld name:%@ price:%@ location:%@",
-                  (long)randomMenu.menuId,
-                  randomMenu.name,
-                  randomMenu.price,
-                  randomMenu.location);
+- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+     if (motion == UIEventSubtypeMotionShake) {
+         if (self.currentShakeStatus == showResult) {
+             return;
+         }
+         self.request = [[MenuRequest alloc] init];
+         NSArray *allMenus = [self.request getAllMenus];
+         if([allMenus count] == 0) {
+             [SVProgressHUD showErrorWithStatus: @"还没有选项，请先添加一些"];
+         }else{
+             Menu *randomMenu = allMenus[arc4random() % [allMenus count]];
+             self.resultImage.image = randomMenu.image;
+             self.resultName.text = randomMenu.name;
+             self.resultPrice.text = randomMenu.price;
+             self.resultLocation.text = randomMenu.location;
         }
     }
+}
+
+#pragma mark - ClickButtonAction
+
+- (IBAction)clickShakeAgain:(id)sender {
+    [self cleanUIForShakeBegin];
+}
+
+- (IBAction)clickConfirm:(id)sender {
+    [SVProgressHUD showSuccessWithStatus: @"确定成功"];
 }
 
 @end
